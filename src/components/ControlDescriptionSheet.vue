f941deda34c35bfb887b3d1b22e97d5867037317
Per Liedman <per.liedman@dotnetmentor.se>
2019-01-01 18:04:36 +0100

    Include control description symbols

--- src/components/ControlDescriptionSheet.vue
+++ src/components/ControlDescriptionSheet.vue
@@ -15,12 +15,12 @@
       <tr v-for="(c, i) in course.controls" :key="i">
         <td>{{i > 0 ? i : ''}}</td>
         <td>{{c.code}}</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><img v-if="c.description.C" :src="`iof-2004/${c.description.C}.svg`" /></td>
+        <td><img v-if="c.description.D" :src="`iof-2004/${c.description.D}.svg`" /></td>
+        <td><img v-if="c.description.E" :src="`iof-2004/${c.description.E}.svg`" /></td>
+        <td><img v-if="c.description.F" :src="`iof-2004/${c.description.F}.svg`" /></td>
+        <td><img v-if="c.description.G" :src="`iof-2004/${c.description.G}.svg`" /></td>
+        <td><img v-if="c.description.H" :src="`iof-2004/${c.description.H}.svg`" /></td>
       </tr>
     </tbody>
   </table>
@@ -45,5 +45,7 @@ export default {
     text-align: center;
     border: 2px solid black;
     color: black;
+    min-width: 2.5em;
+    padding: 0.2em;
   }
 </style>
