bc958e20ddb687f0757888142076a0369f24a18d
Per Liedman <per.liedman@dotnetmentor.se>
2019-01-12 11:48:44 +0100

    Print as PDF or SVG

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -22,6 +22,12 @@
     </mu-expansion-panel>
     <mu-expansion-panel :expand="panel === 'print'" @change="togglePanel('print')">
       <div slot="header">{{$t('menus.print')}}</div>
+        <mu-flex class="select-control-row">
+          <mu-radio value="pdf" v-model="printMedia" label="PDF"></mu-radio>
+        </mu-flex>
+        <mu-flex class="select-control-row">
+          <mu-radio value="svg" v-model="printMedia" label="SVG"></mu-radio>
+        </mu-flex>
       <div slot="action">
         <mu-button slot="action" color="primary" @click="print">
           <mu-icon v-if="!totalPrintJobs" left value="print"/>
@@ -37,10 +43,9 @@
 import Vue from 'vue'
 import ControlDescriptionSheet from './ControlDescriptionSheet.vue'
 import { ocadToSvg } from 'ocad2geojson'
-import PDFDocument from 'pdfkit'
-import SVGtoPDF from 'svg-to-pdfkit'
-import blobStream from 'blob-stream'
 import { saveAs } from 'file-saver'
+
+import { courseMapToSvg, courseMapToPdf } from '../print'
 
 export default {
   components: { ControlDescriptionSheet },
@@ -53,6 +58,7 @@ export default {
   data () {
     return {
       panel: 'courses',
+      printMedia: 'pdf',
       currentPrintJob: 0,
       totalPrintJobs: 0
     }
@@ -64,53 +70,18 @@ export default {
     print () {
       const ocadFile = this.map.file
       const crs = ocadFile.getCrs()
-      const jobs = this.event.courses.filter(c => c.controls.length > 0).map(course => () => new Promise((resolve) => {
-        const mmToPt = 2.83465
-        const scaleFactor = crs.scale / 10000 // 10000 should be the course's print scale setting
-
-        const svg = ocadToSvg(ocadFile)
-        const mapGroup = svg.querySelector('g')
-        const courseSvg = course.toSvg()
-        mapGroup.appendChild(courseSvg)
-        const bounds = course.bounds()
-        // Padding (could be a setting)
-        ;[-20, -20, 20, 20].forEach((x, i) => bounds[i] += x / scaleFactor)
-        // TODO: A4 page in millimeters, this should be configurable
-        const mapWidthPt = Math.min(595, Math.ceil((bounds[2] - bounds[0]) * mmToPt * scaleFactor))
-        const mapHeightPt = Math.min(842, Math.ceil((bounds[3] - bounds[1]) * mmToPt * scaleFactor))
-        svg.setAttribute('width', mapWidthPt)
-        svg.setAttribute('height', mapHeightPt)
-        // svg.setAttribute('viewBox', `0 0 ${bounds[2] - bounds[0]} ${bounds[3] - bounds[1]}`)
-        const transform = `scale(${mmToPt / 100 * scaleFactor}) translate(${-bounds[0] * 100}, ${bounds[3] * 100})`
-        mapGroup.setAttribute('transform', transform)
-
-        const doc = new PDFDocument()
-        const stream = doc.pipe(blobStream())
-
-        doc.fillColor('black')
-          .fillOpacity(1)
-          .strokeColor('black')
-          .strokeOpacity(1)
-          .lineWidth(1)
-          .undash()
-          .fontSize(12)
-          .font('Helvetica')
-
-        SVGtoPDF(doc, svg, 595 / 2 - mapWidthPt / 2, 842 / 2 - mapHeightPt / 2, {
-          assumePt: true,
-          colorCallback: x => {
-            const color = x && ocadFile.colors.find(c => c && c.rgbArray[0] === x[0][0] && c.rgbArray[1] === x[0][1] && c.rgbArray[2] === x[0][2])
-            return color && color.cmyk && [color.cmyk, x[1]] || x
-          }
-        })
-
-        doc.end()
-        stream.on('finish', () => {
-          const blob = stream.toBlob('application/pdf')
-          saveAs(blob, `${this.event.name}-${course.name}` + '.pdf')
-          resolve()
-        })
-      }))
+      const mapSvg = ocadToSvg(ocadFile)
+      const printFn = this.printMedia === 'pdf'
+        ? course => () => 
+          courseMapToPdf(ocadFile, mapSvg, course, crs)
+          .then(blob => saveAs(blob, `${this.event.name}-${course.name}` + '.pdf'))
+        :  course => () => 
+          Promise.resolve(courseMapToSvg(mapSvg, course, crs))
+          .then(svg => new Blob([svg.outerHTML], {type: 'image/svg+xml; charset=utf-8'}))
+          .then(blob => saveAs(blob, `${this.event.name}-${course.name}` + '.svg'))
+      const jobs = this.event.courses
+        .filter(c => c.controls.length > 0)
+        .map(printFn)
 
       this.currentPrintJob = 0
       this.totalPrintJobs = jobs.length
---
+++ src/print.js
@@ -0,0 +1,55 @@
+import PDFDocument from 'pdfkit'
+import SVGtoPDF from 'svg-to-pdfkit'
+import blobStream from 'blob-stream'
+
+const mmToPt = 2.83465
+
+export function courseMapToSvg (mapSvg, course, crs) {
+  const scaleFactor = crs.scale / 10000 // 10000 should be the course's print scale setting
+  const svg = mapSvg.cloneNode(true)
+
+  const mapGroup = svg.querySelector('g')
+  const courseSvg = course.toSvg()
+  mapGroup.appendChild(courseSvg)
+  const bounds = course.bounds()
+  // Padding (could be a setting)
+  ;[-20, -20, 20, 20].forEach((x, i) => bounds[i] += x / scaleFactor)
+  // TODO: A4 page in millimeters, this should be configurable
+  const mapWidthPt = Math.min(595, Math.ceil((bounds[2] - bounds[0]) * mmToPt * scaleFactor))
+  const mapHeightPt = Math.min(842, Math.ceil((bounds[3] - bounds[1]) * mmToPt * scaleFactor))
+  svg.setAttribute('width', mapWidthPt)
+  svg.setAttribute('height', mapHeightPt)
+  // svg.setAttribute('viewBox', `0 0 ${bounds[2] - bounds[0]} ${bounds[3] - bounds[1]}`)
+  const transform = `scale(${mmToPt / 100 * scaleFactor}) translate(${-bounds[0] * 100}, ${bounds[3] * 100})`
+  mapGroup.setAttribute('transform', transform)
+
+  return svg
+}
+
+export function courseMapToPdf (ocadFile, mapSvg, course, crs) {
+  return new Promise((resolve) => {
+    const svg = courseMapToSvg(mapSvg, course, crs)
+    const mapWidthPt = svg.getAttribute('width')
+    const mapHeightPt = svg.getAttribute('height')
+    const doc = new PDFDocument()
+    const stream = doc.pipe(blobStream())
+
+    doc
+      .fontSize(12)
+      .font('Helvetica')
+
+    SVGtoPDF(doc, svg, 595 / 2 - mapWidthPt / 2, 842 / 2 - mapHeightPt / 2, {
+      assumePt: true,
+      colorCallback: x => {
+        const color = x && ocadFile.colors.find(c => c && c.rgbArray[0] === x[0][0] && c.rgbArray[1] === x[0][1] && c.rgbArray[2] === x[0][2])
+        return color && color.cmyk && [color.cmyk, x[1]] || x
+      }
+    })
+
+    doc.end()
+    stream.on('finish', () => {
+      const blob = stream.toBlob('application/pdf')
+      resolve(blob)
+    })
+  })
+}
