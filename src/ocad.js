3e060e2747bc63e358eece4bd480000bd875d6ef
Per Liedman <per.liedman@dotnetmentor.se>
2019-04-07 09:06:28 +0200

    Fix handling control codes > 100, better error handling

--- src/ocad.js
+++ src/ocad.js
@@ -30,12 +30,15 @@ const toControl = f => {
 
   const type = Number(f.properties.objectString.substring(0, 2))
   let kind
+  let codeOffset
   switch (type) {
     case 0: 
       kind = 'start'
       break
     case 10:
+    case 11:
       kind = 'normal'
+      codeOffset = (type - 10) * 100
       break
     case 30:
       kind = 'finish'
@@ -53,13 +56,27 @@ const toControl = f => {
     return desc
   }, {})
 
+  let code = defs[0]
+  if (!isNaN(code)) {
+    code = Number(code) + codeOffset
+  }
+
   const coord = f.geometry.coordinates
   const mmCoord = [coord[0] / 100, coord[1] / 100]
 
-  return new Control(controlId++, kind, defs[0], mmCoord, description)
+  return new Control(controlId++, kind, code, mmCoord, description)
 }
 
 const toCourse = (event, allControls, s) => {
-  const controls = s._pairs.map(({ value }) => allControls.find(c => c.code === value))
+  const controls = s._pairs.map(({ value }) => {
+    const c = allControls
+      .find(c => c.code == value)
+    if (!c) {
+      console.warn(`Could not find control ${value} for course ${s._first}.`)
+    }
+
+    return c
+  })
+  .filter(c => c)
   return new Course(event, event.idGenerator.next(), s._first, controls, 10000) //TODO: scale
 }
